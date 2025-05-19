51e6021c13d4219a048694ba7c817648a143b060
AchinthaUmayanga <140151908+achintha-umayanga@users.noreply.github.com>
2025-05-19 14:59:05 +0530

    dasda

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import './App.css'
+import CommentSection from './components/comments-section'
+
+function App() {
+
+  return (
+    <main className="min-h-screen p-4 md:p-8 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-bold mb-8">Discussion</h1>
+      <div className="bg-white dark:bg-gray-950 rounded-lg shadow p-6">
+        <CommentSection />
+      </div>
+    </main>
+  )
+}
+
+export default App
